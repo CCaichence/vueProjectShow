922b23169b6dea5794261d191e76b9213622824b
CCaichence <371171561@qq.com>
2020-10-30 09:52:49 +0800

    '初始化项目'

---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <TabBar>
+        <TabBarItem path="/home" activeColor="red">
+            <template v-slot:item-icon>
+                <img src="../assets/img/tabbar/home.png" alt="">
+            </template>
+            <template v-slot:item-icon-active>
+                <img src="../assets/img/tabbar/home_active.png" alt="">
+            </template>
+            <template v-slot:item-text>
+                <div>首页</div>
+            </template>
+        </TabBarItem>
+        <TabBarItem path="/category" activeColor="red">
+            <template v-slot:item-icon>
+                <img src="../assets/img/tabbar/classification.png" alt="">
+            </template>
+            <template v-slot:item-icon-active>
+                <img src="../assets/img/tabbar/classification_active.png" alt="">
+            </template>
+            <template v-slot:item-text>
+                <div>分类</div>
+            </template>
+        </TabBarItem>
+        <TabBarItem path="/cart" activeColor="red">
+            <template v-slot:item-icon>
+                <img src="../assets/img/tabbar/cart.png" alt="">
+            </template>
+            <template v-slot:item-icon-active>
+                <img src="../assets/img/tabbar/cart_active.png" alt="">
+            </template>
+            <template v-slot:item-text>
+                <div>购物车</div>
+            </template>
+        </TabBarItem>
+        <TabBarItem path="/profile" activeColor="red">
+            <template v-slot:item-icon>
+                <img src="../assets/img/tabbar/profile.png" alt="">
+            </template>
+            <template v-slot:item-icon-active>
+                <img src="../assets/img/tabbar/profile_active.png" alt="">
+            </template>
+            <template v-slot:item-text>
+                <div>我的</div>
+            </template>
+        </TabBarItem>
+    </TabBar>
+
+</template>
+
+<script>
+    import TabBar from "./TabBar";
+    import TabBarItem from "./TabBarItem";
+    export default {
+        name: "MainTabBar",
+        components: {
+            TabBar,
+            TabBarItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
